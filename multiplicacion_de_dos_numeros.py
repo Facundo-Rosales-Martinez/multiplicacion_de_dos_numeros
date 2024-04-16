9a086f31de4cffc38883af41a30feb237c976f9f
Facundo <frm.code@gmail.com>
2024-04-16 14:11:34 -0300

    hecho el algoritmo basico para multiplicar dos numeros

---
+++ multiplicacion_de_dos_numeros.py
@@ -0,0 +1,11 @@
+nombre = input("¿Cual es tu nombre? :")
+
+multiplicador = int(input("Hola, " + nombre +
+                    " haremos una multiplicación, introduce el multiplicador : "))
+
+multiplicando = int(input("Introduce el multiplicando : "))
+
+resultado = multiplicador * multiplicando
+
+print("El resultado es ", multiplicador,
+      " * ", multiplicando, " = ", resultado)
